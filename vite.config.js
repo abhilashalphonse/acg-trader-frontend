3c6002961ac7e284af1413c6a627c0caf8ed6cdc
Abhilash Alphonse <abhilashalphonse.dev@outlook.com>
2026-09-15 04:20:05 +0100

    Optimize Lightweight Charts dependency

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   optimizeDeps: {
-    include: ['@gocharting/chart-sdk'],
+    include: ['lightweight-charts'],
   },
 })
